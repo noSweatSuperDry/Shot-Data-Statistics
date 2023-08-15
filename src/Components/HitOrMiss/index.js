afadf8fa16e7705732f4be7b934da508e2f57cf5
Zahid Abdullah <zaevanphy@gmail.com>
2023-08-15 13:52:30 +0300

    Hit Miss

--- src/Components/HitOrMiss/index.js
+++ src/Components/HitOrMiss/index.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { PieChart, Pie, Cell, Legend } from "recharts";
+import "./style.css";
 
 class HitOrMiss extends Component {
   render() {
@@ -8,8 +10,8 @@ class HitOrMiss extends Component {
     let totalSessions = 0;
     totalSessions = this.props.shots ? this.props.shots.length : 0;
     if (this.props.shots) {
-      this.props.shots.forEach(shot => {
-        shot.shots.forEach(shotDetail => {
+      this.props.shots.forEach((shot) => {
+        shot.shots.forEach((shotDetail) => {
           if (shotDetail.hit) {
             totalHits++;
           } else {
@@ -18,20 +20,56 @@ class HitOrMiss extends Component {
         });
       });
     }
+    let hitPercent = 0;
+    hitPercent = ((totalHits / (totalHits + totalMisses)) * 100).toFixed(2);
+
+    const pieChartData = [
+      { name: "Hit", value: totalHits },
+      { name: "Miss", value: totalMisses },
+    ];
+
+    const COLORS = ["#0088FE", "#FF8042"];
 
     return (
-      <div> 
+      <div>
+        <h2>Hit and Miss Ratio</h2>
         {this.props.shots == null ? (
           <div>
             <h1>No data</h1>
           </div>
         ) : (
-          <div>
-            <h2>Hit and Miss Ratio</h2>
-            <p>Total Sessions: {totalSessions}</p>
-            <p>Total Hits: {totalHits}</p>
-            <p>Total Misses: {totalMisses}</p>
-            <p>Hit Miss Ratio: {(totalHits / (totalHits + totalMisses) * 100).toFixed(2)}%</p>
+          <div className="splitHalf">
+            <div className="side text">
+              <p>Total Sessions: {totalSessions}</p>
+              <p>Total Hits: {totalHits}</p>
+              <p>Total Misses: {totalMisses}</p>
+              <p>Hit Miss Ratio: {hitPercent}%</p>
+            </div>
+            <div className="side">
+              <PieChart width={300} height={300}>
+                <Pie
+                  data={pieChartData}
+                  cx={150}
+                  cy={150}
+                  outerRadius={60} // Adjust the outer radius to make the pie smaller
+                  fill="#8884d8"
+                  label
+                >
+                  {pieChartData.map((entry, index) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={COLORS[index % COLORS.length]}
+                    />
+                  ))}
+                </Pie>
+                <Legend
+                  verticalAlign="bottom"
+                  height={30}
+                  wrapperStyle={{ fontSize: "12px" }}
+                />{" "}
+                {/* Decrease font size */}
+              </PieChart>
+            </div>
           </div>
         )}
       </div>
@@ -39,9 +77,9 @@ class HitOrMiss extends Component {
   }
 }
 
-const mapStateToProps = (state) => {    
+const mapStateToProps = (state) => {
   return {
-    shots: state.shots.data
+    shots: state.shots.data,
   };
 };
 
